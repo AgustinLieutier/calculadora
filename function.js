6d6a5eaa2a2ba25066c4d398de40013e79dfc1ee
AgustinLieutier <sittinlieutier@gmail.com>
2022-08-31 14:55:30 -0300

    variables js

---
+++ function.js
@@ -0,0 +1,19 @@
+function init(){
+    var resultado = document.getElementById("resultado");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var division = document.getElementById("division");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var multiplicacion = document.getElementById("multiplicacion");
+    var tres = document.getElementById("tres");
+    var dos = document.getElementById("dos");
+    var uno = document.getElementById("uno");
+    var resta = document.getElementById("resta");
+    var igual = document.getElementById("igual");
+    var clear = document.getElementById("clear");
+    var cero = document.getElementById("cero");
+    var suma = document.getElementById("suma");
+}
