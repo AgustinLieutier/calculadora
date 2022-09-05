8afbf639e3162b1b0cff1fa41a1a5230033920b3
MaxiMoya2708 <maximo.moya2708@gmail.com>
2022-09-04 23:39:37 -0300

    register js

---
+++ js/register.js
@@ -0,0 +1,16 @@
+const name = document.getElementsById("nombre")
+const surname = document.getElementsById("apellido")
+const email = document.getElementsById("correo")
+const password = document.getElementsById("contraseña")
+const form =document.getElementById("form")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if(!regexEmail.test(email.value)){
+        alert("El email no es valido")
+    }
+    if(password.value.length < 8){
+        alert("La contraseña no es valida")
+    }
+}
